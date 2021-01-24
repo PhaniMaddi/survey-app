410a0b630037e0df21d2966a5f21cfa62f42925a
Phanindra Maddi <phanindra.maddi@phenompeople.com>
2021-01-24 21:12:29 +0530

    add firebase hosting

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || "8000";
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
 
-app.use(express.static(__dirname + ""));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.post("/survey", (req, res) => {
